54c4b9b6669a32dddca6ca0e4725e065e4c7ad15
Katherine Byers <kat.byers13@gmail.com>
2024-11-21 16:57:21 -0600

    aesthetic changes

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var i = 0;
 var txt = 'Hello World!'
 var speed = 50; 
 var element = document.getElementById("typewriter-target");
+
+cursorBlink();
 setTimeout(typeWriter, 500);
 
 function typeWriter() {
@@ -15,4 +17,32 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
+}
+
+function cursorBlink(){
+    let textArray = txt.split("");
+    let allWords = txt.split(" ");
+    let textLen = textArray.length;
+    
+    const wordLen = allWords.map((word) => {
+        return word.length;
+    })
+    
+    let cursorTimings = {
+        duration: 700,
+        iterations: Infinity,
+        easing: 'cubic-bezier(0,.26,.44,.93)'
+    }
+    
+    document.querySelector("#typewriter-cursor").animate([
+        {
+            opacity: 0
+        },
+        {
+            opacity: 0, offset: 0.7
+        },
+        {
+            opacity: 1
+        }
+    ], cursorTimings);
 }
